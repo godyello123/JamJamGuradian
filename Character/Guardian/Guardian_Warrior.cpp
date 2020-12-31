9f010c966039f6d18f65601e38c229e216311165
godyello123 <godyello123@gmail.com>
2020-12-31 21:12:45 +0900

    Warrior Base Setting

--- Character/Guardian/Guardian_Warrior.cpp
+++ Character/Guardian/Guardian_Warrior.cpp
@@ -2,4 +2,51 @@
 
 
 #include "Guardian_Warrior.h"
+#include "Summoner.h"
+#include "../../Animation/Guardian/Anim_Warrior.h"
 
+void AGuardian_Warrior::AttackEnable(bool bEnable)
+{
+}
+
+bool AGuardian_Warrior::IsAttack() const
+{
+	return false;
+}
+
+void AGuardian_Warrior::LevelUP(ELevelUpType eType)
+{
+}
+
+void AGuardian_Warrior::BeginPlay()
+{
+}
+
+void AGuardian_Warrior::Tick(float DeltaTime)
+{
+}
+
+void AGuardian_Warrior::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+}
+
+float AGuardian_Warrior::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	return 0.0f;
+}
+
+void AGuardian_Warrior::Attack()
+{
+}
+
+void AGuardian_Warrior::SearchTarget()
+{
+}
+
+void AGuardian_Warrior::CheckDistance()
+{
+}
+
+void AGuardian_Warrior::AttackToTarget()
+{
+}
