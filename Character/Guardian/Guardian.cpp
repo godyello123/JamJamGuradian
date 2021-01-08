8bbc8476a1de01c16aacb58d173f7d6684d1f883
godyello123 <godyello123@gmail.com>
2021-01-09 03:27:35 +0900

    수정

--- Character/Guardian/Guardian.cpp
+++ Character/Guardian/Guardian.cpp
@@ -9,7 +9,7 @@
 AGuardian::AGuardian()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	TICKON;
 
 	Summoner = nullptr;
 
